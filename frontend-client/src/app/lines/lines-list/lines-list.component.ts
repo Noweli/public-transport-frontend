121ae5a224a1cd7d508cb8f0cce7accce3f1b9e1
Patryk Lewon <patryklewonn@gmail.com>
2024-01-02 16:37:04 +0100

    Add LinesListComponent and route
    Created a new component called LinesListComponent and added its corresponding route. The components' files such as .html and .ts files are added, and a route for the component is established in app.routes.ts file.

---
+++ frontend-client/src/app/lines/lines-list/lines-list.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-lines-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './lines-list.component.html',
+  styleUrl: './lines-list.component.scss'
+})
+export class LinesListComponent {
+
+}
