7461573358ff0c12fd8d0168a9bc9e1dae5f22c5
Patryk Lewon <patryklewonn@gmail.com>
2024-01-03 20:18:49 +0100

    Add ScheduleHelper class
    A new helper class called ScheduleHelper has been added to the project. This class contains a method to convert a given string input into a corresponding day of the week, making use of a pre-defined map for the conversion.

---
+++ frontend-client/src/app/helpers/schedule-helper.ts
@@ -0,0 +1,25 @@
+export class ScheduleHelper {
+  private static readonly dayMap = new Map([
+    ["0", "Sunday"],
+    ["1", "Monday"],
+    ["2", "Tuesday"],
+    ["3", "Wednesday"],
+    ["4", "Thursday"],
+    ["5", "Friday"],
+    ["6", "Saturday"]
+  ]);
+
+  public static convertScheduleToDays(input: string): string {
+    if (!input || !input.includes(",")) {
+      return '';
+    }
+
+    const scheduleArray = input.split(",");
+
+    return scheduleArray.map((value: string) => this.getDayBasedOnInput(value)).join(",");
+  }
+
+  private static getDayBasedOnInput(input: string): string {
+    return this.dayMap.get(input);
+  }
+}
