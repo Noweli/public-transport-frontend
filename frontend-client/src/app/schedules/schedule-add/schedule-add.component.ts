5c897af773a5005afc0928106f1b9c403627da43
Patryk Lewon <patryklewonn@gmail.com>
2024-01-04 22:25:18 +0100

    Add form for scheduling in schedule-add component
    Implemented a reactive form for scheduling in the schedule-add component. This includes the ability to toggle a recurring schedule and select specific days for the recurrence. The form collects necessary data such as date and time for the schedule and can be submitted. The form is initialized in the component's `ngOnInit` method.

--- frontend-client/src/app/schedules/schedule-add/schedule-add.component.ts
+++ frontend-client/src/app/schedules/schedule-add/schedule-add.component.ts
@@ -1,10 +1,14 @@
-import {Component, ViewChild} from '@angular/core';
-import {FormsModule, NgForm} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatButtonModule} from "@angular/material/button";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatCheckboxModule} from "@angular/material/checkbox";
+import {CommonModule} from "@angular/common";
+import {ScheduleHelper} from "../../helpers/schedule-helper";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
 
 @Component({
   selector: 'app-schedule-add',
@@ -15,15 +19,47 @@ import {MatCheckboxModule} from "@angular/material/checkbox";
     MatDividerModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   templateUrl: './schedule-add.component.html',
   styleUrl: './schedule-add.component.scss'
 })
-export class ScheduleAddComponent {
-  @ViewChild('scheduleForm') scheduleForm: NgForm = null!;
+export class ScheduleAddComponent implements OnInit {
+  private isEditMode: boolean = false;
+  private id: number = null!;
+  protected recipeForm: FormGroup = null!;
+  protected readonly ScheduleHelper = ScheduleHelper;
 
   constructor() {
-    // this.scheduleForm.valu
+  }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  onSubmit(): void {
+    console.log(this.recipeForm);
+  }
+
+  private initializeForm() {
+    this.recipeForm = new FormGroup({
+      isRecurring: new FormControl(false),
+      recurringDays: this.getDaysFormArray(),
+      date: new FormControl(),
+      time: new FormControl()
+    });
+  }
+
+  private getDaysFormArray(): FormArray {
+    let formArray: FormArray = new FormArray<any>([]);
+    for (let i = 0; i < ScheduleHelper.dayMap.size; i++) {
+      formArray.push(new FormControl(false))
+    }
+
+    return formArray;
   }
 }
